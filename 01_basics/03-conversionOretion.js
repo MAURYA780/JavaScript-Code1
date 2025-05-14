42a7285a5eeb7b38f5fa884781571f0c37fb7fc1
MAURYA780 <91670698+MAURYA780@users.noreply.github.com>
2025-05-14 13:07:08 +0000

    conversion or operation

---
+++ 01_basics/03-conversionOretion.js
@@ -0,0 +1,20 @@
+let score="33abc";
+
+console.log(typeof score);
+
+let valueInNumber=Number(score);
+
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+// 33= number
+// "33"= string
+// "33abc"= NaN
+// true= 1; false= 0;
+
+let LogedIn="";
+let boleanLogedIn=Boolean(LogedIn)
+console.log( boleanLogedIn);
+
+// 1=true; 0=false;
+// ""= false; "vivek"=true
