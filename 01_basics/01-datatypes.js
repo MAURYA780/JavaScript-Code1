38d3100a97fa56224bd175ea75234038fb350294
MAURYA780 <91670698+MAURYA780@users.noreply.github.com>
2025-05-14 12:43:12 +0000

    test.js

---
+++ 01_basics/01-datatypes.js
@@ -0,0 +1,17 @@
+"use strict"//after that treat all js code as newer version
+
+//alert(3+3);//it showing error bcz we are using node js not browser
+
+//number
+//string
+// BigInt
+// Boolean
+// null // stand alone value
+// undefined //value is not assing
+//symbol //unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
